e9b5e51f35a7229e9e4d9c91c5c71556a1793457
Sven Baumann <baumann.sv@gmail.com>
2018-12-21 22:55:33 +0100

    Fix some author validation issues again

--- src/Contao/CsrfUrlBuilderFactory.php
+++ src/Contao/CsrfUrlBuilderFactory.php
@@ -11,7 +11,6 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    contao-community-alliance/url-builder
- * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @copyright  2014-2018 Contao Community Alliance.
  * @license    https://github.com/contao-community-alliance/url-builder/blob/master/LICENSE LGPL-3.0
